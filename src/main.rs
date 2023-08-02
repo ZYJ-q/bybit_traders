f7d646aefbaee6aebc518799920c212564ffd842
ZYJ-q <2979049853@qq.com>
2023-08-02 13:02:28 +0800

    p

--- src/main.rs
+++ src/main.rs
@@ -166,7 +166,7 @@ async fn real_time(
 
         let time_min = Local::now().timestamp_millis();
         let last_time_min = time_min - (1000*60*60*24 * minut_end);
-        if time_minut_id == 720 {
+        if time_minut_id == 1440 {
             time_minut_id = 2;
             if minut_end != 0 {
                 minut_end -= 2
